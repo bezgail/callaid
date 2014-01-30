72728cede8ef83832aad79c0d892e993458d811f
Kidane Weldemariam <kidane.yemane@gmail.com>
2014-01-30 12:43:22 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Callaid::Application.config.secret_key_base = '01fd796e957fc7bf6de9127c61b78cc819342d6f425b6c6708e1c22f76e65a21621c3567ec844ed0aee2bfcee2098ceca872b249d4f9f77eb6be74d2466458e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Callaid::Application.config.session_store :cookie_store, key: '_callaid_session'
